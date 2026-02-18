c38dd9b41cd6562ed002fe3bcb27dde10113114e
lara <laragabrielaoliveira@gmail.com>
2026-02-18 12:44:01 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,10 @@
+numero = 250 # variável int
+numero_quebrado = 2.56 # variável float
+joao_ta_dormindo = True # variavel boolena
+texto = "Aula inicial de programação" # variavel de texto
+
+
+print(joao_ta_dormindo)
+print(numero_quebrado)
+print(texto)
+print(numero)
---
+++ aula02.py
@@ -0,0 +1,10 @@
+numero = 980 # variável int
+numero_quebrado = 4.67 # variável float
+eu_to_dormindo = True # variavel boolena
+linha = "segunda aula de programação" # variavel de texto
+
+
+print(numero)
+print(numero_quebrado)
+print(linha)
+print(eu_to_dormindo)
