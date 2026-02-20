bcd3913cf1ae43826417028cd6043a1f9739f140
lara <laragabrielaoliveira@gmail.com>
2026-02-20 09:52:18 -0300

    aula dia 20

---
+++ matematica.py
@@ -0,0 +1,12 @@
+numero1 = int (input ("digite o valor"))
+numero2 = int (input ("digite o valor"))
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+divisao = numero1/numero2
+multiplicacao = numero1 * numero2
+
+print("A soma é:", soma)
+print("A subtracao e:", subtracao)
+print("A divisao e:",divisao)
+print("A multiplicacao e:", multiplicacao)
