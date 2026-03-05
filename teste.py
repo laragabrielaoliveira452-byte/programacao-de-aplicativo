7a5157b87fb3f3402e48a10d60247095c4ee2c0e
lara g <laragabrielaoliveira452@gmail.com>
2026-03-05 10:12:53 -0300

    dia 05/03

---
+++ teste.py
@@ -0,0 +1,17 @@
+comprimento = input("o comprimento está entre 180cm e 12cm")
+
+if comprimento == "sim":
+    print ("comprimento correto:") 
+elif comprimento == "não": 
+    print ("reprovado: problema no comprimento")
+else :
+    print ("reprovado.")
+
+    largura = input ("a largura está estre 5 a 6cm?: ")
+
+    if largura == "sim": 
+        print("peça aprovado")
+    elif largura =="não":
+
+    else :
+        print ("reprovado") 
